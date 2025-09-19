d02409a00d75567e7b06bfff0fccebb72c9c6b0d
Zerfass <czerfass@ndc.nasa.gov>
2025-09-19 11:18:09 -0700

    Add .ipynb to make animations of currents and compairspeeds.ipynb with bootstrapping methods and error in speeds, Added functions from these into funcs.py

--- Code/functions/funcs.py
+++ Code/functions/funcs.py
@@ -19,9 +19,16 @@ def RandTrajectories(ds, amount):
     ax.set_ylabel("latitude")
     return fig, ax
 
-def OneTrajectory(ds,index, ax ):
+def OneTrajectory(ds,index, ax, window:int = None , itime:int =None):
     line = ds.at[index,'geometry']
     x,y = line.xy
+    if window !=None: ## adds padding to end of the array for sliding window
+        x= np.array(x)
+        y=np.array(y)
+        nans = np.fill(window,np.nan)
+        x = np.concatenate((x,nans))
+        nans = np.fill(window,np.nan)
+        y = np.concatenate((y,nans))
     ax.plot(x,y)
     ax.set_xlabel("Longitude")
     ax.set_ylabel("latitude")
@@ -70,7 +77,7 @@ def monthly_data(data):
     return monthly_data
 
 def haversine_dist(lat1, lon1, lat2, lon2):
-    """Returns distance between two points in Km"""
+    """Returns distance between two points in Km (always positive)"""
     from math import radians, sin, cos, atan2, sqrt
     # Convert lat and lon to radians
     lat1_rad, lon1_rad = radians(lat1), radians(lon1)
@@ -87,6 +94,30 @@ def haversine_dist(lat1, lon1, lat2, lon2):
     distance = R*c
     return distance
 
+def haversine_dist_with_negetive(lat1, lon1, lat2, lon2):
+    """ONLY to be used when calcuating Distance in X and Y indivigually 
+    Returns distance between two points in Km 
+    - xdirection and negetive y direction are allowed"""
+    from math import radians, sin, cos, atan2, sqrt
+    # Convert lat and lon to radians
+    lat1_rad, lon1_rad = radians(lat1), radians(lon1)
+    lat2_rad, lon2_rad = radians(lat2), radians(lon2)
+ 
+    dist_lon = lon2_rad-lon1_rad
+    dist_lat = lat2_rad-lat1_rad
+ 
+    a = sin(dist_lat/2)**2+cos(lat1_rad)*cos(lat2_rad)*sin(dist_lon/2)**2
+    c = 2*atan2(sqrt(a), sqrt(1-a))
+ 
+    R = 6371.0
+ 
+    distance = R*c
+    if lat1 > lat2: ## manually checks if Values should be negetive 
+        distance = -distance
+    if lon1 >lon2:
+        distance =-distance
+    return distance
+
 def Palmyra_obj():
     """Returns cords of Palymra as a point"""
     import shapely as shp
@@ -94,7 +125,7 @@ def Palmyra_obj():
 
 def Palmyra_plot(ax):
     Palmyra = Palmyra_obj()
-    ax.scatter(Palmyra.x,Palmyra.y, marker  = "*", color = "r")
+    ax.scatter(Palmyra.x,Palmyra.y, marker  = "*", color = "r", label = "Palmyra")
     return ax 
 
 def distance_from_Palymra(ds):
@@ -119,7 +150,7 @@ def samplefreq(ds):
     ds["SampleFreq"] = ds["Diff_days"]/ds["numpoints"]*24 ## gives average rate of data in hours
     return ds
 
-def add_distance_collumns(data):
+def add_distance_collumns(data = gpd.GeoDataFrame)-> gpd.GeoDataFrame:
     """Adds distance dfad traveled in x and y directions, also provides a list of all points"""
     delx_list = []
     dely_list = []
@@ -154,8 +185,8 @@ def add_distance_collumns(data):
 
         for n in range(0,len(x_coords)-1): ##this has to be one less because using n+1
             m = int(n+1)
-            xdist = haversine_dist(x_coords[n], y_coords[n], x_coords[m],y_coords[n])## gives just the x distance traveled
-            ydist = haversine_dist(x_coords[n], y_coords[n], x_coords[n],y_coords[m]) ## gives just the y distance traveled
+            xdist = haversine_dist_with_negetive(x_coords[n], y_coords[n], x_coords[m],y_coords[n])## gives just the x distance traveled
+            ydist = haversine_dist_with_negetive(x_coords[n], y_coords[n], x_coords[n],y_coords[m]) ## gives just the y distance traveled
             xydist = haversine_dist(x_coords[n], y_coords[n], x_coords[m],y_coords[m]) ##gives distance in both xy
             x_distkm.append(xdist) 
             y_distkm.append(ydist)
@@ -185,10 +216,11 @@ def add_distance_collumns(data):
 
     return data, delx_long, dely_long
 
-def add_time_collumns(data):
+def add_time_collumns(data = gpd.GeoDataFrame)-> gpd.GeoDataFrame:
     """Function to add a list of the times of each GPS point with the Form: 2021-07-02-6:00 
     so it can be indexed into .nc file
     This must already use samplefreq()
+    Add column name: "timelist"
     """
     samplefrequencies = data["SampleFreq"]
     startdate = data["MinOfDate"]
@@ -355,7 +387,7 @@ def Add_Delta_speeds(ds = gpd.GeoDataFrame):
     ds["delta_speed"] = delta_speeds
     return ds
 
-def interp_currents(data,vo,uo):
+def Add_interp_currents(data,vo,uo):
     from scipy.interpolate import interpn 
     def closest_point(data, lat, lon, depth, time):
         nearest = data.sel(latitude = lat, longitude = lon, depth = depth,time = time, method = "nearest") 
@@ -434,7 +466,7 @@ def Rolling_mean(x = list,windowsize = int):
     """Insert a list, produces a rolling mean with specified window size
     returns x, the same size"""
     line  = pd.DataFrame({"speed" : x})
-    line = line["speed"].rolling(windowsize, center=True).mean()
+    line = line["speed"].rolling(windowsize, center=True,min_periods=1).mean()
     line  = line.to_list()
     return line
 
@@ -446,6 +478,157 @@ def OneTrajectory(ax, ds= gpd.GeoDataFrame,index= int ):
     ax.set_xlabel("Longitude")
     ax.set_ylabel("latitude")
     return ax
+
+def Rolling_avg_no_nan(list, window):
+    x = Rolling_mean(list, window)
+    x = np.array(x)
+    masked = ~np.isnan(x)
+    x = x[masked]
+    return x
+
+def Add_avg_speed(ds :gpd.GeoDataFrame)->gpd.GeoDataFrame:
+    ds.columns
+    avg_xspeed = []
+    avg_yspeed = []
+    avg_speed = []
+
+    for i in range(len(ds)):
+        xspeed = ds.at[i,"x_speed"]
+        yspeed = ds.at[i,"y_speed"]
+        speed = ds.at[i,"xy_speed"]
+        #speed1 = (xspeed**2 +yspeed**2)**(1/2)
+
+        xspeed = Rolling_avg_no_nan(xspeed, 6)
+        yspeed = Rolling_mean(yspeed,6)
+        speed = Rolling_mean(speed,6)
+        avg_xspeed.append(xspeed)
+        avg_yspeed.append(yspeed)
+        avg_speed.append(speed)
+
+    ds["avg_xspeed"] = avg_xspeed
+    ds["avg_yspeed"] = avg_yspeed
+    ds["avg_speed"] = avg_speed
+    return ds
+
+def Add_Delta_speeds(ds = gpd.GeoDataFrame, Average =False, Direction = False):
+    """Provides columns deltax_speed, deltay_speed, delta_speed. 
+    Which are the diffence between the dFAD speeds and model speeds
+    If direction is False Takes absolute of vectors. 
+    if average speed is True: Calcuates the rolling speed avage 
+    returns the dataset with added columns"""
+
+    delta_xspeeds = []
+    delta_yspeeds = []
+    delta_speeds = []
+
+    for i in range(len(ds)):
+        # X Values
+        mapped_x = ds["mapped_u"][i]
+        dfad_x = ds["x_speed"][i]
+        if Average == True:
+            dfad_x = ds["avg_xspeed"][i]
+
+        deltax = mapped_x[:-1] - dfad_x ## Magnitude and direction
+        if Direction == False:
+            deltax = (np.abs(mapped_x[:-1]) - np.abs(dfad_x)) # comparing magnitude only ## if want to use relative include /(np.abs(dfad_x))
+        
+        #Y values
+        mapped_y = ds["mapped_v"][i]
+        dfad_y = ds["y_speed"][i]
+        if Average == True:
+            dfad_y = ds["avg_yspeed"][i]
+        
+        deltay = mapped_y[:-1] - dfad_y ## Magnitude and direction
+        if Direction == False:
+            deltay = (np.abs(mapped_y[:-1]) - np.abs(dfad_y)) # comparing magnitude only
+        
+        #speed 
+        mapped = ds["mapped_speed"][i]
+        dfad = ds["xy_speed"][i]
+        if Average == True:
+            dfad = ds["avg_speed"][i]
+        
+        delta = mapped[:-1] - dfad ## Magnitude and direction
+        if Direction == False: 
+            delta = (np.abs(mapped[:-1]) - np.abs(dfad))# comparing magnitude only
+    
+
+        delta_xspeeds.append(deltax)
+        delta_yspeeds.append(deltay)
+        delta_speeds.append(delta)
+    if Average == True:
+        ds["deltax_speed_avg"] = delta_xspeeds
+        ds["deltay_speed_avg"] = delta_yspeeds
+        ds["delta_speed_avg"] = delta_speeds
+    else:
+        ds["deltax_speed"] = delta_xspeeds
+        ds["deltay_speed"] = delta_yspeeds
+        ds["delta_speed"] = delta_speeds
+    return ds
+
+## Gets a list of the delta speeds for the Statistic 
+def list_of_delta_speeds(ds, average = False):
+    delxs = []
+    delys = []
+    delspeeds = []
+    if average == True:
+        for i in range(len(ds)):
+            delx = ds["deltax_speed_avg"][i]
+            dely = ds["deltay_speed_avg"][i]
+            delspeed = ds["delta_speed_avg"][i]
+            delxs.extend(delx)
+            delys.extend(dely)
+            delspeeds.extend(delspeed)
+    if average == False:        
+        for i in range(len(ds)):
+            delx = ds["deltax_speed"][i]
+            dely = ds["deltay_speed"][i]
+            delspeed = ds["delta_speed"][i]
+            delxs.extend(delx)
+            delys.extend(dely)
+            delspeeds.extend(delspeed)
+    return delxs, delys, delspeeds
+
+def bootstrapping_each_box(lat, lon, variable, bins = 10):
+    """Returns values and error bars, produces 1000 bootstrapping samples"""
+    from scipy.stats import binned_statistic_2d
+
+    Values,xedges, yedges, binnumber = binned_statistic_2d(lat, lon, variable, statistic= "mean", bins = bins, expand_binnumbers = True)
+    binnumber = binnumber -1
+
+    ## making empty array to store the speeds into
+    list_array = np.empty((bins,bins), dtype= object)
+    for i in range(bins):
+        for j in range(bins):
+            list_array[i,j] = []
+
+    ##Placing speeds into correct list 
+    for i in range(binnumber.shape[1]):
+        lati = binnumber[0,i]
+        loni = binnumber[1,i]
+        value = delxs[i]
+        list_array[lati,loni].append(value)
+    ## Doing the bootstrapping
+    nbootstrapps = 1000
+    samplelist = []
+    for k in range(nbootstrapps):
+
+        meanarray = np.zeros((bins,bins))
+        for i in range(bins):
+            for j in range(bins):
+                speeds = list_array[i,j]
+                randomi = np.random.randint(0,len(speeds),len(speeds))
+                sampledspeeds = []
+                for n in range(len(randomi)):
+                    sampledspeeds.append(speeds[randomi[n]])
+                meanarray[i,j] = np.mean(sampledspeeds)
+        samplelist.append(meanarray)
+
+    samplelist = np.array(samplelist)
+    errors = np.percentile(samplelist,95,axis = 0)
+    errorbars = errors - Values
+   
+    return Values, errorbars, xedges, yedges, errors
 
 class plotting:
     def __init__():
